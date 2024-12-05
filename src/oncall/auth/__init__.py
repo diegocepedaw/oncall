070fd1be933b3f2af631715bab7d4c28458ad097
Diego Cepeda <dcepeda@linkedin.com>
2024-12-05 15:40:40 -0600

    implement SSO auth with debug module

--- src/oncall/auth/__init__.py
+++ src/oncall/auth/__init__.py
@@ -13,6 +13,7 @@ from .. import db
 
 logger = logging.getLogger('oncall.auth')
 auth_manager = None
+sso_auth_manager = None
 
 
 def debug_only(function):
@@ -186,6 +187,11 @@ def authenticate_application(auth_token, req):
 
 
 def _authenticate_user(req):
+    # pass the req to the sso_auth_manager so it can check if it has valid SSO headers
+    if sso_auth_manager and sso_auth_manager.authenticate(req):
+        req.context['user'] = sso_auth_manager.authenticate(req)
+        return
+    # fall back to session based login
     session = req.env['beaker.session']
     try:
         req.context['user'] = session['user']
@@ -238,6 +244,7 @@ def init(application, config):
     global check_calendar_auth_by_id
     global debug_only
     global auth_manager
+    global sso_auth_manager
     global authenticate_user
 
     if config.get('debug', False):
@@ -255,6 +262,10 @@ def init(application, config):
         check_calendar_auth = lambda x, y, **kwargs: True
         check_calendar_auth_by_id = lambda x, y: True
         debug_only = lambda function: function
+
+    if config.get('sso_module'):
+        sso_auth = importlib.import_module(config['sso_module'])
+        sso_auth_manager = getattr(sso_auth, 'Authenticator')(config)
 
     if config.get('docs') or config.get('require_auth'):
         # Replace login_required decorator with identity function for autodoc generation
